350a5f2cb3eb79f6edaae3bd3167c84d690c2eed
Josh Holman <git@jolman.me>
2022-12-31 16:58:26 -0800

    wip

--- modules/home/wm/default.nix
+++ modules/home/wm/default.nix
@@ -1,11 +1,34 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    mkOption
+    types
+    ;
+  cfg = config.wm.hypr;
+in {
   imports = [
     ./hyprlock.nix
     ./hyprland.nix
     ./waybar.nix
     ./fuzzel.nix
-    # ./hyprpanel.nix
     ./swaync.nix
-    # ./ashell.nix
   ];
+
+  options.wm.hypr = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Enables customized Hyprland DE.
+      '';
+    };
+  };
+
+  config = {
+  };
 }
