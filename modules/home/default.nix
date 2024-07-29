dad46dd5de0192715ed28754ebc8ddb8d41990a7
josh <joshuaholman5@gmail.com>
2024-07-28 21:09:45 -0700

    moved home imports into modules/home/default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,8 +1,18 @@
 {...}:
 {
   imports = [
+    ./zsh.nix
+    ./fuzzel.nix
+    ./kitty.nix
+    ./dunst.nix
+    ./swaylock.nix
+    ./swayidle.nix
+    ./git.nix
+    ./hyprland.nix
+    ./nixvim/default.nix
     ./hypridle.nix
     ./hyprlock.nix
+    ./pluginlist.nix
 
   ];
 }
