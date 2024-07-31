f12adacb65ac5cd1fd5cea3378f7e403b05c8976
josh <joshuaholman5@gmail.com>
2024-07-31 16:26:54 -0700

    moved gtk and catppuccin

---
+++ modules/home/theming.nix
@@ -0,0 +1,36 @@
+{inputs, ...}: {
+  
+  imports = [
+    inputs.catppuccin.homeManagerModules.catppuccin
+  ];
+
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
+    pointerCursor.enable = true;
+  };
+
+  xdg.enable = true;
+  gtk = {
+    enable = true;
+    catppuccin = {
+      flavor = "mocha";
+      tweaks = ["normal"];
+      icon.enable = true;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "kvantum";
+    style.name = "kvantum";
+  };
+
+  # home.pointerCursor = {
+  #   gtk.enable = true;
+  #   x11.enable = true;
+  #   package = pkgs.bibata-cursors;
+  #   name = "Bibta-Modern-Classic";
+  #   size = 25;
+  # };
+}
