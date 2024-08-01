517990b2f9d5e4b0fe42eb410e710e8bf8aea8bc
josh <joshuaholman@gmail.com>
2024-08-01 00:31:30 +0000

    added .nix

--- hosts/workstation/configuration.nix
+++ hosts/workstation/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../../modules/nixos/default
+      ../../modules/nixos/default.nix
       ./hardware-configuration.nix
     ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
