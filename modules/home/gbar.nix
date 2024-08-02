7d097256cc1b5a14f1e4eab5a4693b84f890ca6a
josh <joshuaholman5@gmail.com>
2024-08-02 16:06:16 -0700

    changed gBar config

--- modules/home/gbar.nix
+++ modules/home/gbar.nix
@@ -4,13 +4,14 @@
   programs.gBar = {
     enable = true;
     config = {
-      Location = "L";
+      Location = "T";
       EnableSNI = true;
       SNIIconSize = {
         Discord = 26;
         Zoom = 26;
       };
-      WorkspaceSymbols = [ " " " " ];
+      WorkspaceSymbols = [ "1" "2" "3" "4" "5" "6" "7" "8" "9"];
+
     };
   };
 
