d17b42d857d9c7f1f9738074d18e8bab6acfd42d
josh <joshuaholman@gmail.com>
2024-09-04 19:42:30 -0700

    removed swayidle from hyprland startup I think

--- modules/home/wm/hyprland.nix
+++ modules/home/wm/hyprland.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: let
   startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
     gBar bar 0 &
-    ${pkgs.swayidle}/bin/swayidle &
+    # ${pkgs.swayidle}/bin/swayidle &
     ${pkgs.dunst}/bin/dunst &
     nm-applet --indicator &
     blueman-applet &
