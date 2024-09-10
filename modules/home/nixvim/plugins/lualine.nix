24407c9789412062f6452ff71caff73145287a46
josh <joshuaholman5@gmail.com>
2024-09-09 18:39:38 -0700

    did a nix flake update

--- modules/home/nixvim/plugins/lualine.nix
+++ modules/home/nixvim/plugins/lualine.nix
@@ -1,46 +1,55 @@
 {...}: {
   programs.nixvim.plugins.lualine = {
     enable = true;
-    theme = "catppuccin";
-    globalstatus = true;
 
-    extensions = ["oil"];
 
-    sections = {
-      lualine_a = ["mode"];
-      lualine_b = ["branch" "diff"];
-      lualine_c = ["diagnostics"];
-      lualine_x = ["encoding" "fileformat" "filetype"];
-      lualine_y = ["filesize" "progress"];
-      lualine_z = ["filename"];
+
+    settings = {
+      options = {
+        globalstatus = true;
+        theme = "catppuccin";
+      };
+
+      extensions = ["oil"];
+
+      sections = {
+        lualine_a = ["mode"];
+        lualine_b = ["branch" "diff"];
+        lualine_c = ["diagnostics"];
+        lualine_x = ["encoding" "fileformat" "filetype"];
+        lualine_y = ["filesize" "progress"];
+        lualine_z = ["filename"];
+      };
+
+      tabline = {
+        lualine_a = ["buffers"];
+        lualine_b = [""];
+        lualine_c = [""];
+        lualine_x = [""];
+        lualine_y = [""];
+        lualine_z = [""];
+      };
+
+      winbar = {
+        lualine_a = [''
+          require(lspsaga.symbol.winbar).get_bar()''];
+        lualine_b = [""];
+        lualine_c = [""];
+        lualine_x = [""];
+        lualine_y = [""];
+        lualine_z = [""];
+      };
+
+      inactiveWinbar = {
+        lualine_a = [""];
+        lualine_b = [""];
+        lualine_c = [""];
+        lualine_x = [""];
+        lualine_y = [""];
+        lualine_z = ["filename"];
+      };
+
     };
 
-    tabline = {
-      lualine_a = ["buffers"];
-      lualine_b = [""];
-      lualine_c = [""];
-      lualine_x = [""];
-      lualine_y = [""];
-      lualine_z = [""];
-    };
-
-    winbar = {
-      lualine_a = [''
-        require(lspsaga.symbol.winbar).get_bar()''];
-      lualine_b = [""];
-      lualine_c = [""];
-      lualine_x = [""];
-      lualine_y = [""];
-      lualine_z = [""];
-    };
-
-    inactiveWinbar = {
-      lualine_a = [""];
-      lualine_b = [""];
-      lualine_c = [""];
-      lualine_x = [""];
-      lualine_y = [""];
-      lualine_z = ["filename"];
-    };
   };
 }
