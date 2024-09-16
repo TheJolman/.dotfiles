516852cde4863b32be314a57caa94e298217de89
josh <joshuaholman@gmail.com>
2024-09-16 10:59:50 -0700

    removed plasma stuff from main

--- flake.nix
+++ flake.nix
@@ -40,11 +40,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
   };
 
   outputs = {
