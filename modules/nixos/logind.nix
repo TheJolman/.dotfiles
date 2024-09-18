05c7ac847dea5c30a23b57fdb85bd4e725e62e89
josh <joshuaholman5@gmail.com>
2024-09-17 20:13:12 -0700

    finalized logind settings

--- modules/nixos/logind.nix
+++ modules/nixos/logind.nix
@@ -1,6 +1,8 @@
 {...}: {
-  # suspend or hybrid-sleep
-  services.logind.lidSwitch = "hybrid-sleep";
-  services.logind.lidSwitchDocked = "ignore";
-  services.logind.powerKey = "hibernate";
+  services.logind = {
+    lidSwitch = "suspend"; # could also use hybrid-sleep here
+    lidSwitchDocked = "ignore";
+    powerKey = "hibernate";
+    powerKeyLongPress = "reboot";
+  };
 }
