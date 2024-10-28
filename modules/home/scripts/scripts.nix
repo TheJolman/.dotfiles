43d0d572addbeb9f974123ecd82d178a2ed79e38
josh <joshuaholman5@gmail.com>
2024-10-27 18:56:56 -0700

    fixed script name

--- modules/home/scripts/scripts.nix
+++ modules/home/scripts/scripts.nix
@@ -3,7 +3,7 @@
   home.packages = [
     (pkgs.writeScriptBin "cinit" (builtins.readFile ./cinit.sh))
 
-    (pkgs.writeScriptBin "nsinit" (builtins.readFile ./nstemp.sh))
+    (pkgs.writeScriptBin "nsinit" (builtins.readFile ./nsinit.sh))
 
     (pkgs.writeScriptBin "calc" ''
       #usr/bin/env/bash
