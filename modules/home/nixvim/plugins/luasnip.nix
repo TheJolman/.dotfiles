6e7f92ca24ce2e2b875dbfd8eaf17776261431b7
josh <joshuaholman5@gmail.com>
2024-12-02 13:03:47 -0800

    luasnip!

--- modules/home/nixvim/plugins/luasnip.nix
+++ modules/home/nixvim/plugins/luasnip.nix
@@ -1,8 +1,15 @@
 {pkgs, ...}: {
-  programs.nixvim.plugins.luasnip = {
-    enable = true;
-    settings = {enable_autosnippets = true;};
+  programs.nixvim = {
+    plugins = {
+      friendly-snippets.enable = true;
+      luasnip = {
+        enable = true;
+        settings = {
+          enable_autosnippets = true;
+        };
+      };
+    };
+    extraConfigLua = builtins.readFile ./lua/luasnip.lua;
+    extraPackages = [pkgs.lua51Packages.jsregexp];
   };
-
-  programs.nixvim.extraPackages = [pkgs.lua51Packages.jsregexp];
 }
