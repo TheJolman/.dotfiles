1d499bbcb2f910fc23f07b784953d745af02c5b0
josh <joshuaholman5@gmail.com>
2024-12-25 23:11:29 -0800

    more neorg

--- modules/home/nixvim/plugins/neorg.nix
+++ modules/home/nixvim/plugins/neorg.nix
@@ -2,15 +2,24 @@
   programs.nixvim = {
     plugins.neorg = {enable = true;};
     extraConfigLuaPost = ''
+      -- lua config (extraConfigLua doesn't work in the neorg module)
       require("neorg").setup({
-          load = {
-              ["core.defaults"] = {},
-              ["core.concealer"] = {
-                  config = {
-                      icon_preset = "varied",
-                  },
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {
+            config = {
+              icon_preset = "varied",
+            },
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
               },
-          }
+              index = "index.norg",
+            },
+          },
+        },
       })
 
     '';
