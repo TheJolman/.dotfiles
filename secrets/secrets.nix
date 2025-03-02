44f0d848c85f2caca0f93dc18791d46f880df573
josh <joshuaholman5@gmail.com>
2025-03-01 17:49:25 -0800

    added public ssh on ws

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   # users
-  josh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJn2NlE78fst1VWVLFLVAoy5imiANy87AutdsMpCg81";
-  users = [josh];
+  josh_fw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJn2NlE78fst1VWVLFLVAoy5imiANy87AutdsMpCg81";
+  josh_ws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMj+QUIzL1UnI/5cHFzVrrmF9stKFFRl3lkeXgGD4mJr";
+  users = [josh_fw josh_ws];
 
   # systems
   framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN4x/Z5KJNonztSMxAKIakOmowXcyZ5o+ij4MmaLCPYC";
