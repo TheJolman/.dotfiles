3c846520a902dc857477ca32d9a9864e6334955c
josh <joshuaholman5@gmail.com>
2025-03-19 19:41:47 -0700

    made vscode usable!

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode.fhs;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
       denoland.vscode-deno
@@ -9,6 +8,7 @@
       vscjava.vscode-java-pack
       vscjava.vscode-gradle
       ms-vscode.cpptools
+      llvm-vs-code-extensions.vscode-clangd
       ms-vscode.makefile-tools
       ms-vscode.cmake-tools
       mesonbuild.mesonbuild
@@ -27,5 +27,112 @@
       golang.go
       rust-lang.rust-analyzer
     ];
+    enableExtensionUpdateCheck = false;
+    userSettings = builtins.fromJSON ''
+      {
+          "editor.semanticHighlighting.enabled": true,
+          "terminal.integrated.minimumContrastRatio": 1,
+          "window.titleBarStyle": "custom",
+          "workbench.colorTheme": "Catppuccin Mocha",
+          "catppuccin.accentColor": "pink",
+          "catppuccin.customUIColors": {
+            "mocha": {
+              "statusBar.foreground": "accent"
+            }
+          },
+
+          "files.autoSave": "off",
+          "editor.minimap.enabled": false,
+          "editor.fontFamily": "'CaskaydiaCove Nerd Font', monospace",
+          "editor.fontSize": 14,
+          "editor.fontLigatures": true,
+          "terminal.integrated.fontFamily": "'CaskaydiaCove Nerd Font', monospace",
+          "terminal.integrated.fontLigatures.enabled": true,
+          "editor.renderWhitespace": "trailing",
+          "editor.lightbulb.enabled": "off",
+          "window.menuBarVisibility": "toggle",
+          "window.titleBarStyle": "native",
+          "workbench.activityBar.location": "default",
+          "editor.scrollbar.vertical": "hidden",
+          "editor.scrollbar.horizontal": "hidden",
+          "editor.wordWrap": "on",
+          "editor.tabSize": 2,
+          "editor.insertSpaces": true,
+          "editor.formatOnSave": true,
+          "editor.foldingStrategy": "indentation",
+          "editor.showFoldingControls": "mouseover",
+          "telemetry.telemetryLevel": "off",
+          "workbench.enableExperiments": false,
+          "extensions.autoUpdate": false,
+          "extensions.ignoreRecommendations": true,
+
+          "update.mode": "none",
+          "vim.leader": "<space>",
+          "vim.overrideCopy": true,
+          "vim.useSystemClipboard": true,
+          "vim.normalModeKeyBindings": [
+              {
+                  "before": [
+                      "<leader>",
+                      "w"
+                  ],
+                  "commands": [
+                      "workbench.action.files.save"
+                  ]
+              },
+              {
+                  "before": ["A-b"],
+                  "commands": [
+                      "workbench.action.toggleSidebarVisibility"
+                  ]
+              },
+              {
+                  "before": ["<leader>", "f", "b"],
+                  "commands": [
+                      "workbench.view.search"
+                  ]
+              },
+              {
+                  "before": ["-"],
+                  "commands": [
+                      "workbench.view.explorer"
+                  ]
+              },
+              {
+                  "before": ["g", "d"],
+                  "commands": [
+                      "editor.action.revealDefinition"
+                  ]
+              },
+              {
+                  "before": [
+                      "K"
+                  ],
+                  "commands": [
+                      "editor.action.showHover"
+                  ]
+              }
+          ],
+          "vim.visualModeKeyBindings": [
+              {
+                  "before": [
+                      ">"
+                  ],
+                  "commands": [
+                      "editor.action.indentLines"
+                  ]
+              },
+              {
+                  "before": [
+                      "<"
+                  ],
+                  "commands": [
+                      "editor.action.outdentLines"
+                  ]
+              }
+          ]
+      }
+
+    '';
   };
 }
