18ce1b11497119cd8e699b3336df32e3deb69bc9
josh <joshuaholman5@gmail.com>
2025-04-16 23:44:34 -0700

    im dumb??

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -16,7 +16,7 @@
         autoSetupRemote = true;
       };
       gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
+      user.signingkey = "~/.ssh/id_ed25519";
       commit.gpgsign = true;
     };
     ignores = [
