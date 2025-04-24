dd9717c55b09f99128535a9746dfae234e4758ec
josh <joshuaholman5@gmail.com>
2025-04-24 11:43:19 -0700

    kinda worky vm

--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -1,14 +1,4 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    #   qemu
-    #   virt-manager
-    # win-virtio
-    # OVMF
-    # swtpm
-    # tpm-tools
-    # tpm2-tools
-  ];
-
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = ["josh"];
 
