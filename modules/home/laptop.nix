0e057cc8a15ba02dcc9abcef4b2136f5cb9d15a8
Josh Holman <josh@jolman.me>
2025-06-21 19:51:06 -0700

    framework hardware module

--- modules/home/laptop.nix
+++ modules/home/laptop.nix
@@ -1,3 +1,6 @@
-{...}: {
+{pkgs, ...}: {
   wayland.windowManager.hyprland.settings.input.accel_profile = "adaptive";
+  home.packages = with pkgs; [
+    powertop
+  ];
 }
--- modules/nixos/framework.nix
+++ modules/nixos/framework.nix
@@ -1,3 +1,6 @@
-{...}: {
+{nixos-hardware, ...}: {
+  imports = [
+    nixos-hardware.nixosModules.framework-13-7040-amd
+  ];
   services.fwupd.enable = true;
 }
