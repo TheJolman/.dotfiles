7461df6c01f182121b2098fa189625e79e2e7300
Josh Holman <josh@jolman.me>
2025-07-02 20:28:31 -0700

    simplify interception tools

--- modules/nixos/interception-tools.nix
+++ modules/nixos/interception-tools.nix
@@ -5,34 +5,31 @@
     plugins = with pkgs.interception-tools-plugins; [
       dual-function-keys
       caps2esc
-      ralt2hyper
     ];
     udevmonConfig = ''
       - JOB: >
-          "${pkgs.interception-tools}/bin/intercept -g $DEVNODE
-          | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml
-          | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc
-          | ${pkgs.interception-tools-plugins.ralt2hyper}/bin/ralt2hyper
-          | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+          ${pkgs.interception-tools}/bin/intercept -g $DEVNODE |
+          ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc |
+          ${pkgs.interception-tools}/bin/uinput -d $DEVNODE
         DEVICE:
           EVENTS:
-            EV_KEY: [KEY_CAPSLOCK, KEY_ESC, KEY_RIGHTSHIFT, KEY_RIGHTALT]
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
     '';
   };
 
   # Create the dual-function-keys configuration file
   # https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
-  environment.etc."dual-function-keys.yaml".text = ''
-    TIMING:
-      TAP_MILLISEC: 200
-      DOUBLE_TAP_MILLISEC: 150
-
-    MAPPINGS:
-      # - KEY: KEY_CAPSLOCK
-      #   TAP: KEY_ESC
-      #   HOLD: KEY_LEFTCTRL
-      - KEY: KEY_RIGHTSHIFT
-        TAP: KEY_BACKSPACE
-        # HOLD: KEY_BACKSPACE
-  '';
+  # environment.etc."dual-function-keys.yaml".text = ''
+  #   TIMING:
+  #     TAP_MILLISEC: 200
+  #     DOUBLE_TAP_MILLISEC: 150
+  #
+  #   MAPPINGS:
+  #     # - KEY: KEY_CAPSLOCK
+  #     #   TAP: KEY_ESC
+  #     #   HOLD: KEY_LEFTCTRL
+  #     - KEY: KEY_RIGHTSHIFT
+  #       TAP: KEY_BACKSPACE
+  #       # HOLD: KEY_BACKSPACE
+  # '';
 }
