71a5c40c153a11941ab0928955f1c5bac150dbba
Josh Holman <josh@jolman.me>
2025-07-07 15:45:22 -0700

    yazi thang

--- modules/home/shell/yazi.nix
+++ modules/home/shell/yazi.nix
@@ -3,4 +3,15 @@
     enable = true;
     enableZshIntegration = true;
   };
+
+  programs.zsh.initContent = ''
+    function y() {
+      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+      yazi "$@" --cwd-file="$tmp"
+      if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+      fi
+      rm -f -- "$tmp"
+    }
+  '';
 }
--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -64,14 +64,14 @@
       autoload -Uz compinit && compinit
       eval "$(pixi completion --shell zsh)"
 
-      function y() {
-        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-        yazi "$@" --cwd-file="$tmp"
-        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-          builtin cd -- "$cwd"
-        fi
-        rm -f -- "$tmp"
-      }
+      # function y() {
+      #   local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+      #   yazi "$@" --cwd-file="$tmp"
+      #   if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      #     builtin cd -- "$cwd"
+      #   fi
+      #   rm -f -- "$tmp"
+      # }
     '';
   };
 
