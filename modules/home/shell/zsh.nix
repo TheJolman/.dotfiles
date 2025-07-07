a6fc2483d8303c3260bb0b594764e978845d4447
Josh Holman <josh@jolman.me>
2025-07-07 15:50:33 -0700

    remove dead code

--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -61,24 +61,9 @@
     };
 
     initContent = ''
-      autoload -Uz compinit && compinit
       eval "$(pixi completion --shell zsh)"
-
-      # function y() {
-      #   local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-      #   yazi "$@" --cwd-file="$tmp"
-      #   if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-      #     builtin cd -- "$cwd"
-      #   fi
-      #   rm -f -- "$tmp"
-      # }
     '';
   };
-
-  # programs.starship = {
-  #   enable = true;
-  #   enableZshIntegration = true;
-  # };
 
   programs.carapace = {
     enable = true;
