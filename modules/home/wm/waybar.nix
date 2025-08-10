c470328ad721801c3d322a9108c951c2b9cbbd81
Josh Holman <josh@jolman.me>
2025-08-10 09:34:06 -0700

    switch from hyprpanel

--- modules/home/wm/waybar.nix
+++ modules/home/wm/waybar.nix
@@ -9,7 +9,16 @@
         height = 30;
         modules-left = ["custom/logo" "hyprland/workspaces"];
         modules-center = ["clock"];
-        modules-right = ["tray" "network" "wireplumber" "battery" "cpu" "temperature" "memory"];
+        modules-right = [
+          "tray"
+          "network"
+          "wireplumber"
+          "battery"
+          "cpu"
+          "temperature"
+          "memory"
+          "custom/notification"
+        ];
 
         "custom/logo" = {
           format = "";
@@ -68,6 +77,17 @@
         battery = {
           format-icons = [" " " " " " " " " "];
           format = "{icon} {capacity}%";
+        };
+
+        "custom/notification" = {
+          tooltip = false;
+          format = "{icon}";
+          return-type = "json";
+          exec-if = "which swaync-client";
+          exec = "swaync-client -swb";
+          on-click = "swaync-client -t -sw";
+          on-click-right = "swaync-client -t -sw";
+          escape = true;
         };
       };
     };
