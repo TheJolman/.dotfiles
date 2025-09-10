29051a8421784c8fde394dfd796062ac7e8e8749
josh <joshuaholman5@gmail.com>
2025-09-09 20:57:38 -0700

    more theme settings

--- modules/home/theming.nix
+++ modules/home/theming.nix
@@ -33,6 +33,18 @@
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
   };
 
   qt = {
