239a6c2af50a9067e95d1a87cd12045c44d66b3b
Josh Holman <git@jolman.me>
2025-12-05 11:25:25 -0800

    update mimeApps list

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -133,11 +133,13 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
+      "application/pdf" = "firefox.desktop";
       "text/html" = "firefox.desktop";
       "x-scheme-handler/http" = "firefox.desktop";
       "x-scheme-handler/https" = "firefox.desktop";
       "x-scheme-handler/about" = "firefox.desktop";
       "x-scheme-handler/unknown" = "firefox.desktop";
+      "x-scheme-handler/discord" = "vesktop.desktop";
     };
   };
 }
