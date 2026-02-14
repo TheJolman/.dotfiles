bc6625884d915feb27ee3256380ab0b72d719c18
Josh Holman <git@jolman.me>
2026-02-14 13:03:37 -0800

    scaffold nixos laptop options

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -1,6 +1,38 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    mkOption
+    types
+    ;
+  cfg = config.laptop;
+in {
   imports = [
     ./tlp.nix
     ./fprintd.nix
   ];
+
+  options.laptop = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Enables laptop specific settings, like TLP power profiles and lid switch behavior.
+      '';
+    };
+    enableFingerprintReader = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Enables fprintd.
+      '';
+    };
+  };
+
+  config = {
+  };
 }
