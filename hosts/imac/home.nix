1ac73afb87192b0462baa8ed9c9d6daff3145f40
Josh Holman <git@jolman.me>
2026-03-29 21:53:27 -0700

    update imports

--- hosts/imac/home.nix
+++ hosts/imac/home.nix
@@ -4,12 +4,11 @@
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   imports = [
-    ../../modules/home/shell/zsh.nix
     ../../modules/home/direnv.nix
     ../../modules/home/git.nix
-    ../../modules/home/nixvim/default.nix
+    ../../modules/home/nixvim
     ../../modules/home/catppuccin.nix
-    ../../modules/home/fzf.nix
+    ../../modules/home/scripts
   ];
 
   # Let Home Manager install and manage itself.
