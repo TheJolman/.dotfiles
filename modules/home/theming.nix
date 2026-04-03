6577d8349b77f23050c07cc1e68eee1ef62aa523
josh <git@jolman.me>
2026-04-03 10:30:30 -0700

    comment out broken gtk theming stuff

--- modules/home/theming.nix
+++ modules/home/theming.nix
@@ -19,12 +19,12 @@
 
   gtk = {
     enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+    # theme = {
+    #   name = "Adwaita-dark";
+    #   package = pkgs.gnome-themes-extra;
+    # };
+    # gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    # gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
     # gtk3.extraConfig = {
     #   Settings = ''
     #     gtk-application-prefer-dark-theme=1
